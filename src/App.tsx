a892250a13fcb9e28776cdeb35848b9eeac30c74
zoherjetpurwala <zoher.jetpurwala@gmail.com>
2025-01-20 19:15:39 +0530

    Added loading spinner on main page

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,20 @@ import { UserProvider, useUser } from "./context/UserContext";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 
+const LoadingScreen: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+    </div>
+  );
+};
+
 const AppRoutes: React.FC = () => {
-  const { user } = useUser();
+  const { user, loading } = useUser();
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <Routes>
