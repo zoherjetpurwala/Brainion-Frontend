1d78b9a1c6142eb0fa3fc751e754c8732e2788d9
ZeJsters <zoher.jetpurwala@gmail.com>
2025-02-08 21:53:21 +0530

    Trying another library for embedding

--- src/components/XEmbed.tsx
+++ src/components/XEmbed.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { TwitterTweetEmbed } from 'react-twitter-embed';
+import * as React from "react";
+import { Tweet } from "react-twitter-widgets";
 
 export interface XEmbedProps extends React.HTMLAttributes<HTMLDivElement> {
   url: string;
@@ -14,61 +14,25 @@ export const XEmbed: React.FC<XEmbedProps> = ({
   url,
   width,
   height,
-  linkText = 'View post on X',
+  linkText = "View post on X",
   placeholderImageUrl,
   placeholderDisabled = false,
   style,
   ...divProps
 }) => {
-  const postId = url.substring(url.lastIndexOf('/') + 1).replace(/[?].*$/, '');
-  const [isLoaded, setIsLoaded] = React.useState(false);
+  const postId = url.substring(url.lastIndexOf("/") + 1).replace(/[?].*$/, "");
 
   return (
     <div
       {...divProps}
       style={{
-        overflow: 'hidden',
-        width: width ?? '100%',
-        height: height ?? 'auto',
-        position: 'relative',
+        overflow: "hidden",
+        width: width ?? "100%",
+        height: height ?? "auto",
         ...style,
       }}
     >
-      {!isLoaded && (
-        <div
-          style={{
-            width: '100%',
-            height: height ?? 350,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#f0f2f5',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            borderRadius: 12,
-            animation: 'pulse 1.5s infinite',
-          }}
-        >
-          <p style={{ color: '#657786', fontSize: 14 }}>Loading tweet...</p>
-        </div>
-      )}
-      <TwitterTweetEmbed tweetId={postId} onLoad={() => setIsLoaded(true)} />
+      <Tweet tweetId={postId} />
     </div>
   );
 };
-
-const styles = `
-@keyframes pulse {
-  0% {
-    opacity: 0.6;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0.6;
-  }
-}`;
-
-document.head.insertAdjacentHTML('beforeend', `<style>${styles}</style>`);
